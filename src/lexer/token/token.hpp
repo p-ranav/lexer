48ac47f7060b952993d0403af8db6f718959657e
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2018-11-23 11:33:50 -0500

    Added Lexer token types

--- src/lexer/token/token.hpp
+++ src/lexer/token/token.hpp
@@ -1,12 +1,34 @@
 #pragma once
 #include <string>
 
-using std::string;
+enum TokenType {
+  // Special tokens
+  ILLEGAL, EOF,
+
+  // Literals
+  IDENTIFIER, CHARACTER, STRING, INTEGER, DOUBLE,
+
+  // Operators
+  PLUS, MINUS, STAR, SLASH, PERCENT, BANG,
+  EQUAL, BANG_EQUAL, EQUAL_EQUAL, 
+  GREATER, GREATER_EQUAL, LESSER, LESSER_EQUAL
+
+  // Delimiters
+  DOT, COMMA, COLON, SEMICOLON,
+
+  // Brackets
+  LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, 
+  LEFT_BRACKET, RIGHT_BRACKET,
+
+  // KEYWORDS
+  NULL_, VAR, TRUE, FALSE, IF, ELSE, WHILE, FOR, IN, NOT, 
+  FUNCTION, RETURN, IMPORT
+};
 
 struct Token {
-  string mType;
-  string mLiteral;
-  string mFileName;
+  std::string mType;
+  std::string mLiteral;
+  std::string mFileName;
   int mLine;
   int mCursor;
 };
